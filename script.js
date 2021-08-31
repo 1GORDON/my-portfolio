60e9e2fae89b84c43bba263ed7ada16c0ea2c409
Gordon <gordon.tinyefuza@gmail.com>
2021-08-31 16:48:56 +0300

    Add onclick events

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const myFunction = () => {
-  document.getElementById("drop-down").classList.add("view")
-  document.getElementById("firstPage").classList.add("blurry-background")
-}
+  document.getElementById('drop-down').classList.add('view');
+  document.getElementById('firstPage').classList.add('blurry-background');
+};
 
 const myFunction1 = () => {
-  document.getElementById("drop-down").classList.remove("view")
-  document.getElementById("firstPage").classList.remove("blurry-background")
-}
+  document.getElementById('drop-down').classList.remove('view');
+  document.getElementById('firstPage').classList.remove('blurry-background');
+};
